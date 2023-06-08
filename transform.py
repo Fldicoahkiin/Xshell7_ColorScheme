e79e208c58519ab679a1dfd49cf1bf89c8e5c263
Flacier <flacier@qq.com>
2023-06-08 13:51:05 +0800

    Go through all the files

--- transform.py
+++ transform.py
@@ -1,28 +1,29 @@
 import os
 
-def batch_edit_files():
-    directory = os.path.dirname(__file__)
-    for filename in os.listdir(directory):
-        if filename.endswith(".xcs"):
-            # 修改后缀名为 .scs
-            new_filename = filename[:-4] + ".scs"
-            old_filepath = os.path.join(directory, filename)
-            new_filepath = os.path.join(directory, new_filename)
+def batch_edit_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".xcs"):
+                # 修改后缀名为 .scs
+                new_filename = filename[:-4] + ".scs"
+                old_filepath = os.path.join(root, filename)
+                new_filepath = os.path.join(root, new_filename)
 
-            with open(old_filepath, 'r') as old_file:
-                lines = old_file.readlines()
-            
-            # 修改第一行为 [Color Scheme]
-            lines[0] = "[Color Scheme]\n"
-            
-            # 删除第21行以及后面的内容
-            if len(lines) >= 21:
-                del lines[20:]
+                with open(old_filepath, 'r') as old_file:
+                    lines = old_file.readlines()
 
-            with open(new_filepath, 'w') as new_file:
-                new_file.writelines(lines)
-            
-            # 删除原文件
-            os.remove(old_filepath)
+                # 修改第一行为 [Color Scheme]
+                lines[0] = "[Color Scheme]\n"
 
-batch_edit_files()
+                # 删除第21行以及后面的内容
+                if len(lines) >= 21:
+                    del lines[20:]
+
+                with open(new_filepath, 'w') as new_file:
+                    new_file.writelines(lines)
+
+                # 删除原文件
+                os.remove(old_filepath)
+
+directory = os.path.dirname(__file__)
+batch_edit_files(directory)
